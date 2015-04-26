c8547975937538f1d19c97e1411b54277c3e268e
DmitrakovDI <clooosha@gmail.com>
2015-04-26 21:50:27 +0300

    Изменен алгоритм определения сломанных секций

--- class_light.py
+++ class_light.py
@@ -2,10 +2,13 @@
 import itertools
 class Light:
 	def __init__(self,):
+		self.color = 'green'
 		self.break_section = [set(),set()]	#список сломанных секциц для каждого циферблата
+		self.change_count_1 = 0
+		self.prev_section = [[], []]
 		self.numbs = [set(), set()]			# список множест предополагаемых цифр
 		self.counter = 0					# кол-во поступивших запросов
-		self.set_number = ({0,1,2,4,5,6}, {2,4}, {0,2,3,4,6}, {0,2,3,5,6}, {1,2,3,5}, {0,1,3,5,6}, {0,1,3,4,5,6}, {0,2,5}, {0,1,2,3,4,5,6}, {0,1,2,3,5,6})
+		self.set_number = ({0,1,2,4,5,6}, {2,5}, {0,2,3,4,6}, {0,2,3,5,6}, {1,2,3,5}, {0,1,3,5,6}, {0,1,3,4,5,6}, {0,2,5}, {0,1,2,3,4,5,6}, {0,1,2,3,5,6})
 
 	def str_in_set(self, str_num):
 		"""Преобразование строки горящих секций в множество"""
@@ -25,60 +28,74 @@ class Light:
 		"""Поиск среди цифр возможных по множеству горящих секций"""
 		return {x for x in range(len(self.set_number)) if len(num - self.set_number[x]) == 0}
 
-	def search_break_section(self, cur_num, cur_numbs):
+	def search_break_section(self, cur_section, cur_numbs):
 		"""Определение сломанных секций."""
 		section = {0,1,2,3,4,5,6}
 		for x in cur_numbs:
-			section &= self.set_number[x]		#Из списка предполагаемых цифр, определяем множество общих секций
-		return section - cur_num			#Вычитаем текущие секции
+			section &= self.set_number[x]									#Из списка предполагаемых цифр, определяем множество общих секций
+		return section - cur_section										#Вычитаем текущие секции
 
 	def analyze(self, color, str_nums = ['0000000', '0000000']):
 		"""Анализируем полученные секции и цвет цветофора"""
-		print (color, str_nums)
+		if self.color == 'red':
+			return ('error', "The red observation should be the last")
 		if color == 'red':
+			self.color = 'red'
 			if self.counter == 0:
 				return ('error', "There isn't enough data") 				#Ошибка, нет данных
 			else:
+				self.numbs[0] &= {self.counter // 10}
+				self.numbs[1] &= {self.counter % 10}
+				if len(self.numbs[0]) == 0 or len(self.numbs[0]) == 0:
+					return ('error', "No solutions found")
+				else:
+					for x in range(len(self.prev_section[0])):
+						self.break_section[0] |= self.search_break_section(self.prev_section[0][x], {y - x for y in self.numbs[0]})
+					for x in range(len(self.prev_section[1])):
+						self.break_section[1] |= self.search_break_section(self.prev_section[1][x], {y - x for y in self.numbs[1]})
 				return (self.counter, self.set_in_str(self.break_section[0]), self.set_in_str(self.break_section[1]))
 		else:
 			#Анализ 1-го циферблата
 			cur_section = self.str_in_set(str_nums[0])
-			print(cur_section)
 			cur_numbs = self.search_number(cur_section)
-			print(cur_numbs)
-			if self.counter == 0:								#Первая данные
+
+			if self.counter == 0:											#Первые данные
 				self.numbs[0] |= set(cur_numbs)
+				self.prev_section[0].append(cur_section)
 
-			if self.numbs[0] != cur_numbs:						#Сменилась цифра на 1-м циферблате, значит на 2-ом текущая 9
-				self.numbs[1] = (9 + self.counter) % 10			#Значит на 2-м текущая 9
-				self.numbs[0] &= {(x + 1) % 10 for x in cur_numbs}	#Пересечение предыдущих предполагаемых цифр и полученных сейчас
+			if cur_section != self.prev_section[0][-1]:						#Сменилась цифра на 1-м циферблате, значит на 2-ом текущая 9
+				self.numbs[1] &= {(9 + self.counter) % 10}					#Значит на 2-м цифра текущая 9, вычесляем стартовую, учитывая ранее найденные
+				if len(self.prev_section[0]) < 10:
+					self.prev_section[0].append(cur_section)
+					self.numbs[0] &= {(x + len(self.prev_section[0]) - 1) % 10 for x in cur_numbs}	#Пересечение предыдущих предполагаемых цифр и полученных сейчас
 
-			cur_break_section = set()
 			if len(self.numbs[0]) == 0:
-				return ('error', "No solutions found")				#Ошибка. Некорректные данные
+				return ('error', "No solutions found")						#Ошибка. Некорректные данные
 			elif len(self.numbs[0]) == 1:
-				cur_break_section = self.search_break_section(cur_num, (self.numbs[0] - counter) % 10)
+				for x in range(len(self.prev_section[0])):
+					self.break_section[0] |= self.search_break_section(self.prev_section[0][x], {y - x for y in self.numbs[0]})
 			else:
-				cur_break_section = self.search_break_section(cur_section, cur_numbs)
-			self.break_section[0] |= cur_break_section
+				self.break_section[0] |= self.search_break_section(cur_section, cur_numbs)
 
 			#Анализ 2-го циферблата
 			cur_section = self.str_in_set(str_nums[1])
+			if len(self.prev_section[1]) < 10:
+				self.prev_section[1].append(cur_section)
+
 			cur_numbs = self.search_number(cur_section)
-			print (cur_section, cur_numbs)
-			if self.counter == 0:				#Первая данные
+
+			if self.counter == 0:											#Первые данные
 				self.numbs[1] |= set(cur_numbs)
 			else:
 				self.numbs[1] &= {(x + self.counter) % 10 for x in cur_numbs}#Пересечение предыдущих предполагаемых цифр и полученных сейчас
 
-			cur_break_section = set()
 			if len(self.numbs[1]) == 0:
-				return ('error', "No solutions found")				#Ошибка. Некорректные данные
+				return ('error', "No solutions found")						#Ошибка. Некорректные данные
 			elif len(self.numbs[1]) == 1:
-				cur_break_section = self.search_break_section(cur_num, (self.numbs[1] - counter) % 10)
+				for x in range(len(self.prev_section[1])):
+					self.break_section[1] |= self.search_break_section(self.prev_section[1][x], {y - x for y in self.numbs[1]})
 			else:
-				cur_break_section = self.search_break_section(cur_section, cur_numbs)
-			self.break_section[1] |= cur_break_section
+				self.break_section[1] |= self.search_break_section(cur_section, cur_numbs)
 
 			self.counter += 1
 		return (list(x[0]*10 + x[1] for x in itertools.product(self.numbs[0], self.numbs[1])), self.set_in_str(self.break_section[0]), self.set_in_str(self.break_section[1]))
@@ -88,3 +105,9 @@ if __name__ == '__main__':
 	print (a.analyze('green', ['1110111', '0011101']))
 	print (a.analyze('green', ['1110111', '0010000']))
 	print (a.analyze('red'))
+	print (a.analyze('green', ['1110111', '0010000']))
+	b = Light()
+	print (b.analyze('green', ['0010010', '1110110']))
+	print (b.analyze('green', ['1110111', '1110010']))
+	c = Light()
+	print (c.analyze('red'))
