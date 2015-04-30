77750919b1c5921a9f0b83ad3246b7c0b2288951
DmitrakovDI <clooosha@gmail.com>
2015-04-30 15:34:12 +0300

    Обновлено readme.md Откорректирована проверка запросов в test_http.py

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # traffic_light
 machinezone.ru
+
+Сервис для задания от machinezone.ru. Написан на Python3. Тестировался на Ubuntu 14.04 tls
+
+Установка:
+	1) создайть каталог /usr/local/traffic_light/
+	2) скопирировать файлы в каталог
+	3) Скопировать из папки /usr/local/traffic_light/service файл traffic_light в /etc/init.d
+	4) регистрация сервиса в системе:
+		sudo update-rc.d traffic_light defaults
+
+Как работает:
+	1)запуск сервиса:
+		service traffic_light start
+	2) остановка сервиса:
+		service traffic_light stop
+	3) Информация о введенных последовательностях храниться в файле:
+		/usr/local/traffic_light/data.pickle
+	4) Сервис работает на порту 8080. Чтобы запустить на нужном порту измените значение в файле traffic_light.py
+	5) Тестовые запросы к сервису приведены в файле test_http.py
+
+Удаление:
+	1) удаление севриса из системы:
+		sudo update-rc.d -f traffic_light remove
+		sudo rm -f /etc/init.d/traffic_light
+	2) удаление каталога:
+		sudo rm -fr /usr/local/traffic_light
+
--- test_http.py
+++ test_http.py
@@ -3,46 +3,65 @@
 import requests
 import json
 
+#Создания последовательности
 url = 'http://127.0.0.1:8080/sequence/create'
 r = requests.post(url)
 print(r.status_code, r.content)
+assert r.status_code == 200
 answer = json.loads(r.content.decode('utf-8'))
-
 sequence = ''
 if 'status' in answer and answer['status'] == 'ok':
 	sequence = answer['response']['sequence']
 else:
 	exit -1
 
+# Данные #1
 url = 'http://127.0.0.1:8080/observation/add'
 payload = {'observation': {'color':'green', 'numbers': ['1110111', '0011101']}, 'sequence':sequence}
 r = requests.post(url, data = json.dumps(payload).encode('utf-8'))
 print(r.status_code, r.content)
+assert r.status_code == 200
+assert json.loads(r.content.decode('utf-8')) == {"response": {"start": [8, 2, 88, 82], "missing": ["0000000", "1000000"]}, "status": "ok"}
 
+# Данные №2
 url = 'http://127.0.0.1:8080/observation/add'
 payload = {'observation': {'color':'green', 'numbers': ['1110111', '0010000']}, 'sequence':sequence}
 r = requests.post(url, data = json.dumps(payload).encode('utf-8'))
 print(r.status_code, r.content)
+assert r.status_code == 200
+assert json.loads(r.content.decode('utf-8')) == {"response": {"start": [8, 2, 88, 82], "missing": ["0000000", "1000010"]}, "status": "ok"}
 
+# Данные №3
 url = 'http://127.0.0.1:8080/observation/add'
 payload = {'observation': {'color':'red'}, 'sequence':sequence}
 r = requests.post(url, data = json.dumps(payload).encode('utf-8'))
 print(r.status_code, r.content)
+assert r.status_code == 200
+assert json.loads(r.content.decode('utf-8')) == {"response": {"start": [2], "missing": ["0000000", "1000010"]}, "status": "ok"}
 
+# Кривой запрос на добавление данных
 url = 'http://127.0.0.1:8080/observation/add'
 payload = 'test=test'
 r = requests.post(url, data = json.dumps(payload).encode('utf-8'))
 print(r.status_code, r.content)
+assert r.status_code == 200
+assert json.loads(r.content.decode('utf-8')) == {"status": "error", "msg": "Bad data: no sequence"}
 
+# Неверное URI
 url = 'http://127.0.0.1:8080/bla_bla'
 payload = 'privet'
 r = requests.post(url, data = json.dumps(payload).encode('utf-8'))
 print(r.status_code, r.content)
+assert r.status_code == 404
 
+# Очистка
 url = 'http://127.0.0.1:8080/clear'
 r = requests.post(url)
 print(r.status_code, r.content)
+assert json.loads(r.content.decode('utf-8')) == {"response": "ok", "status": "ok"}
 
+# Get-запрос
 url = 'http://127.0.0.1:8080'
 r = requests.get(url)
 print(r.status_code, r.content)
+assert r.status_code == 200
